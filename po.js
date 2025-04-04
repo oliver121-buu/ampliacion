663857f9578324b3b2183e543bb1785fadec2698
oliver121-buu <oliblas.10@gmail.com>
2025-04-04 11:00:41 +0200

    confirmar

---
+++ po.js
@@ -0,0 +1,13 @@
+function invertirArray(mi_array){
+    let array2 = [];
+    for(let index = my_array.length-1; index< mi_array.length; i--)
+    {
+        array_invertido.push(mi_array[index]);
+    }
+    return array_invertido;
+}
+
+
+
+
+let aEjemplo = invertirArray(["a","b","c","d","e"]);
